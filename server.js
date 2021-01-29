8e384f9bcef5d5b3ad9b1dd6b811f30a2271b29f
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-01-29 00:58:38 -0800

    Added function calls to switch cases

--- server.js
+++ server.js
@@ -42,43 +42,43 @@ function runsearch() {
     .then((answer) => {
         switch (answer.choice) {
             case "View All Employees":
-
+                viewAllEmployees();
                 break;
             case "View All Employees By Department":
-
+                viewAllEmployeesByDepartment();
                 break;
             case "View All Employees By Manager":
-
+                viewAllEmployeesByManager();
                 break;
             case "Add Employee":
-
+                addEmployee();
                 break;
             case "Remove Employee":
-
+                removeEmployee()
                 break;
             case "Update Employee Role":
-
+                updateEmployeeRole();
                 break;
             case "Update Employee Manager":
-
+                updateEmployeeManager();
                 break;
             case "View All Roles":
-
+                viewAllRoles();
                 break;
             case "Add Role":
-
+                addRole();
                 break;
             case "Remove Role":
-
+                removeRole();
                 break;
             case "View All Departments":
-
+                viewAllDepartments();
                 break;
             case "Add Department":
-
+                addDepartment();
                 break;
             case "Remove Department":
-
+                removeDepartment();
                 break;
             case "Quit":
                 connection.end();
