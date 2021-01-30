9cbf7a6f74f503a7ca2532cc75a069d6538d1c0a
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-01-29 18:38:47 -0800

    Updating schema for FOREIGN KEYS

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,29 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    department_id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (department_id)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    role_id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INT NOT NULL
+    department_id INT,
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    employee_id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (role_id) REFERENCES role(role_id),
+    FOREIGN KEY (manager_id) REFERENCES role(role_id)
 );
 
 SELECT * FROM department;
